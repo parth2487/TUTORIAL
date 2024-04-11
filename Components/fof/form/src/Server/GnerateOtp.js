e6e9627211ea97e9a214280b4719346a9efbc46d
parth2487 <155759012+parth2487@users.noreply.github.com>
2024-04-12 00:51:44 +0530

    Delete Components directory

--- Components/fof/form/src/Server/GnerateOtp.js
+++
@@ -1,9 +0,0 @@
-const otpGenerator = require('otp-generator')
-
-const GnerateOtp=()=>{
-const otp=	otpGenerator.generate(6, { lowerCaseAlphabets:false,upperCaseAlphabets: false, specialChars: false });
-
-return otp
-}
-
-module.exports=GnerateOtp()
